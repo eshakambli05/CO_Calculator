227d89a0d11d3670634285aa8593c603a8b75824
RakshitaKubal <150452903+RakshitaKubal@users.noreply.github.com>
2025-11-08 15:15:52 +0530

    Add files via upload

---
+++ programoutcome.cpp
@@ -0,0 +1,18 @@
+#include "programoutcome.h"
+
+QVector<QString> ProgramOutcome::getPredefinedPOs() {
+    return {
+        "PO1: Engineering knowledge",
+        "PO2: Problem analysis",
+        "PO3: Design/development of solutions",
+        "PO4: Conduct investigations",
+        "PO5: Modern tool usage",
+        "PO6: The engineer and society",
+        "PO7: Environment and sustainability",
+        "PO8: Ethics",
+        "PO9: Individual and team work",
+        "PO10: Communication",
+        "PO11: Project management",
+        "PO12: Life-long learning"
+    };
+}
---
+++ programoutcome.h
@@ -0,0 +1,12 @@
+#ifndef PROGRAMOUTCOME_H
+#define PROGRAMOUTCOME_H
+
+#include <QString>
+#include <QVector>
+
+class ProgramOutcome {
+public:
+    static QVector<QString> getPredefinedPOs();  // 12 predefined POs
+};
+
+#endif // PROGRAMOUTCOME_H
