7eb5a4a9637ecfcfaa10cc8cf721f9c5a56da141
eshakambli05 <eshakambli10@gmail.com>
2025-11-08 14:37:38 +0530

    Delete faculty.cpp

--- faculty.cpp
+++
@@ -1,8 +0,0 @@
-#include "faculty.h"
-
-Faculty::Faculty(const QString& name, const QString& email, const QString& department)
-    : name(name), email(email), department(department) {}
-
-QString Faculty::getName() const { return name; }
-QString Faculty::getEmail() const { return email; }
-QString Faculty::getDepartment() const { return department; }
