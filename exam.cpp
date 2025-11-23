df5fbb3157aa18aec0cc31c4cdf49efe6d3451f8
RakshitaKubal <150452903+RakshitaKubal@users.noreply.github.com>
2025-11-23 17:40:08 +0530

    Delete exam.cpp

--- exam.cpp
+++
@@ -1,8 +0,0 @@
-#include "exam.h"
-
-Exam::Exam(const QString& name) : name(name) {}
-
-QString Exam::getName() const { return name; }
-
-void Exam::setCOMarks(const QString& co, int maxMarks) { coMarks[co] = maxMarks; }
-QMap<QString, int> Exam::getCOMarks() const { return coMarks; }
