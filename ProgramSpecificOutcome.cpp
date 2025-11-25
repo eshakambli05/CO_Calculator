3f0d10f09212e40b58708ee3ca7c0ec1aeb82d35
RakshitaKubal <150452903+RakshitaKubal@users.noreply.github.com>
2025-11-25 19:26:58 +0530

    Delete ProgramSpecificOutcome.cpp

--- ProgramSpecificOutcome.cpp
+++
@@ -1,7 +0,0 @@
-#include "ProgramSpecificOutcome.h"
-
-ProgramSpecificOutcome::ProgramSpecificOutcome(const QString& code, const QString& description)
-    : m_code(code), m_description(description) {}
-
-QString ProgramSpecificOutcome::getCode() const { return m_code; }
-QString ProgramSpecificOutcome::getDescription() const { return m_description; }
